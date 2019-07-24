36d481cfce322561b8565cb1fbf0c2da68c4c520
Liao Guangming <liaoguangming@huawei.com>
2019-07-24 09:19:35 +0800

    improve py_func_def bnf

--- python_lex_yacc/py_func_def.py
+++ python_lex_yacc/py_func_def.py
@@ -1,31 +1,31 @@
 
 tokens = (
-    'NAME', 'TYPE_POINTER', 'TYPE_INT', 'DEF',
+    'NAME', 'TYPE_POINTER', 'DEF', 'COMMENT'
 )
 
 
+def t_COMMENT(t):
+    r"""\#.*"""
+    pass
+
+
 def t_DEF(t):
-    r'def'
-    return t
-
-
-def t_TYPE_INT(t):
-    r'int'
+    r"""def"""
     return t
 
 
 def t_NAME(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    r"""[a-zA-Z_][a-zA-Z0-9_]*"""
     return t
 
 
 def t_TYPE_POINTER(t):
-    r'->'
+    r"""->"""
     return t
 
 
-literals = ",:()"
-t_ignore = ' \t'
+literals = ",:()[]*"
+t_ignore = ' \t\n'
 
 
 def t_error(t):
@@ -39,52 +39,60 @@ lexer = lex.lex()
 
 
 def p_statement_expression(p):
-    """statement : func_def_expression"""
+    """
+    statement : func_def_expression
+    """
     print('p_statement_expression: {}'.format(p.slice))
 
 
 def p_func_def_expression(p):
-    """func_def_expression : DEF NAME '(' arg_list_expression ')' TYPE_POINTER type_expression ':'"""
-    print('p_func_def_expression: {}'.format(p.slice))
-
-
-def p_func_def_expression_1(p):
-    """func_def_expression : DEF NAME '(' arg_list_expression ')' ':'"""
-    print('p_func_def_expression_1: {}'.format(p.slice))
-
-
-def p_func_def_expression_2(p):
-    """func_def_expression : DEF NAME '(' ')' TYPE_POINTER type_expression ':'"""
-    print('p_func_def_expression_2: {}'.format(p.slice))
-
-
-def p_func_def_expression_3(p):
-    """func_def_expression : DEF NAME '(' ')' ':'"""
+    """
+    func_def_expression : DEF NAME '(' arg_list_expression ')' TYPE_POINTER type_expression ':'
+                        | DEF NAME '(' arg_list_expression ')' ':'
+                        | DEF NAME '(' ')' TYPE_POINTER type_expression ':'
+                        | DEF NAME '(' ')' ':'
+    """
     print('p_func_def_expression_3: {}'.format(p.slice))
 
 
 def p_arg_list_expression(p):
-    """arg_list_expression : NAME
-                           | NAME ',' arg_list_expression"""
+    """
+    arg_list_expression : NAME
+                        | NAME ':' type_expression
+                        | arg_list_expression ','
+                        | arg_list_expression ',' arg_list_expression
+    """
     print('p_arg_list_expression: {}'.format(p.slice))
 
 
 def p_type_expression(p):
-    '''type_expression : TYPE_INT'''
+    """
+    type_expression : NAME
+                    | NAME '[' multi_type_expression ']'
+    multi_type_expression : type_expression
+                          | type_expression ',' multi_type_expression
+    """
+    print('type_expression: {}'.format(p.slice))
+
+
+def p_value_expression(p):
+    """
+    value_expression : 
+                     | NAME '[' multi_type_expression ']'
+    """
     print('type_expression: {}'.format(p.slice))
 
 
 def p_error(p):
-    print("Syntax error at '%s'" % p.value)
+    raise Exception("Syntax error at '%s'" % p.value)
 
 
-def test_lexer():
-    data = 'def abc_1_b(a, b) -> int:'
+def test_lexer(data):
     lexer.input(data)
     while True:
         tok = lexer.token()
         if not tok:
-            break  # No more input
+            break
         print(tok.type, tok.value, tok.lineno, tok.lexpos)
 
 
@@ -93,12 +101,23 @@ yacc.yacc(debug=True)
 
 
 def test_yacc():
-    while True:
-        try:
-            s = input('calc > ')   # use input() on Python 3
-        except EOFError:
-            break
-        yacc.parse(s)
+    test_src = [
+        'def abc(a,):',
+        'def abc(a,):',
+        'def abc(a,#xxx()[]\n):',
+        'def abc(a: List[int],):',
+        'def abc() -> int:',
+        'def abc(a, b) -> List[int]:',
+        'def abc(a, b: Union[int, List[float]],) -> Union[int, float]:',
+        'def abc(a,) -> Union[int, List[float]]:',
+        'def abc(a,) -> Union[int, List[float], float]:',
+        'def abc(a,) #xxx()[]\n -> Union[int, List[float], float]:'
+    ]
+    for test_line in test_src:
+        print('#####  test start for: {}'.format(test_line))
+        test_lexer(test_line)
+        yacc.parse(test_line)
+        print('#####  test end for: {}\n'.format(test_line))
 
 
 if __name__ == '__main__':
