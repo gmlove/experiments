51f5566519eb0de40596fd99a12cc45b997b5371
gmlove <gracekinglau@gmail.com>
2020-03-29 13:26:56 +0800

    update test

--- rust/musl-perf/src/main.rs
+++ rust/musl-perf/src/main.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 use rayon::prelude::*;
 use uuid;
 
-fn main() {
+fn test_map_get_perf__with__in_thread_mem_allocation() {
     let mut map = HashMap::new();
 
     for i in 0..1000000 {
@@ -31,5 +31,40 @@ fn main() {
     }).sum();
 
     println!("multi thread sum with map.get: {}ms", start.elapsed().as_millis());
+}
 
+fn test_map_get_perf__without__in_thread_mem_allocation() {
+    let mut map = HashMap::new();
+
+    for i in 0..1000000 {
+        map.insert(i, i);
+    }
+
+    let start = Instant::now();
+    let mut sum = 0;
+    for i in 0..1000000 {
+        sum += *map.get(&i).unwrap_or(&0);
+    }
+    println!("single thread sum(={}) with map.get: {}ms", sum, start.elapsed().as_millis());
+
+    let start = Instant::now();
+    let thread_num = 10usize;
+    let range: Vec<usize> = Range { start: 0usize, end: thread_num }.collect();
+    let sum: usize = range.into_par_iter().map(|_| {
+        let range: Vec<usize> = Range { start: 0, end: 1000000 / thread_num }.collect();
+        let sum: usize = range.iter().map(|i| {
+            *map.get(&i).unwrap_or(&0)
+        }).sum();
+        sum
+    }).sum();
+
+    println!("multi thread sum(={}) with map.get: {}ms", sum, start.elapsed().as_millis());
+}
+
+
+fn main() {
+    println!("test_map_get_perf__with__in_thread_mem_allocation");
+    test_map_get_perf__with__in_thread_mem_allocation();
+    println!("test_map_get_perf__without__in_thread_mem_allocation");
+    test_map_get_perf__without__in_thread_mem_allocation();
 }
